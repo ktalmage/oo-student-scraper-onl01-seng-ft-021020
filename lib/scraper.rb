fd2de7cd492d7c58447c8547c17097e2d39f1b7c
Kyle Talmage <ktalmage85@gmail.com>
2020-02-29 18:59:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,7 @@ class Scraper
     name = student.css(".student-name").text,
     location = student.css(".student-location").text,
     profile_url = student.css("a").attribute("href").value
-    student_hash = {:name => name,
+    student_hash =        {:name => name,
                     :location => location,
                     :profile_url => profile_url}
     scraped_students << student_hash
